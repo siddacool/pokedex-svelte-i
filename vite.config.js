73d490168301990ff1468e1283b012298b409ba9
Siddhesh Mangela <sidthecool007@gmail.com>
2021-07-09 14:24:25 +0530

    gh-pages config added

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { resolve } from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  base: '/pokedex-svelte-i/',
   resolve: {
     alias: {
       constants: resolve('./src/constants'),
